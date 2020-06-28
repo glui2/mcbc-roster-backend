95f48b977584852571034eb67e425bbfc3596332
Unknown <reynatan21@gmail.com>
2020-06-28 12:08:44 +1000

    Use okta jwt verifier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import pgPromise from "pg-promise";
+import { access } from "fs";
+import OktaJwtVerifier from "@okta/jwt-verifier";
 
 export const register = (app: express.Application) => {
   const oidc = app.locals.oidc;
@@ -13,6 +15,13 @@ export const register = (app: express.Application) => {
 
   const pgp = pgPromise();
   const db = pgp(config);
+
+//   const OktaJwtVerifier = require('@okta/jwt-verifier');
+  const oktaJwtVerifier = new OktaJwtVerifier({
+    clientId: process.env.OKTA_CLIENT_ID,
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+  });
+
 
   // define a secure route handler
   app.get("/user/:id", async (req: any, res) => {
@@ -36,15 +45,17 @@ export const register = (app: express.Application) => {
     }
   });
 
-  app.get("/test", oidc.ensureAuthenticated(), async (req: any, res) => {
+  app.get("/test", async (req: any, res) => {
     try {
-      // tslint:disable-next-line:no-console
-      console.log(req);
-      return res.json("Gavin");
-    } catch (err) {
-      // tslint:disable-next-line:no-console
-      console.error(err);
-      res.json({ error: err.message || err });
-    }
+        if (!req.headers.authorization) throw new Error('Authorization header is required');
+
+        const accessToken = req.headers.authorization.trim().split(' ')[1];
+        await oktaJwtVerifier.verifyAccessToken(accessToken, 'api://default');
+        res.json("Reyna");
+      } catch (error) {
+        // tslint:disable-next-line:no-console
+        console.log(error.message);
+        res.json(error.message);
+      }
   });
 };
